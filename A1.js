8aba9c20670c58102b048d6274d34262014bcf89
AaradhyaIsProInCoding <vijaylaxmi22779@gmail.com>
2024-12-17 20:17:57 +0100

    Add files via upload

---
+++ A1.js
@@ -0,0 +1,12 @@
+function palindrom(myString){
+    var input = myString.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+    var reversedInput = input.split('').reverse().join('');
+    if (input === reversedInput) {
+        document.write("<div>" + myString + " is a Palindrome </div>");
+    }
+    else{
+        document.write("<div>" + myString + " is not a Palindrome </div>");
+    }
+}
+
+palindrom("India $$")
